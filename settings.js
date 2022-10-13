b3f7f2e24dabb569167d197e683ff68ef13eea9a
Hasinthyadenuwan <107313544+Hasinthyadenuwan@users.noreply.github.com>
2022-10-13 21:08:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94773696116'] //ur owner number
+global.ownername = "blacky🖤💝" //ur owner name
+global.ytname = "YT: Bp0503" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94773596116']
+global.ownertag = '94773596116' //ur tag number
+global.botnumber = '94773596116' //Add your Bot Number For Log
+global.botname = 'Blacky 🖤💝💫' //ur bot name
+global.ig = "
+https://youtube.com/channel/UCIRbuGg6PIVPgIBBCEtn1nA
+
+
+
+" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCIRbuGg6PIVPgIBBCEtn1nA" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://youtube.com/channel/UCIRbuGg6PIVPgIBBCEtn1nA'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
 global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.author = "blacky 💝🖤" //ur sticker watermark author
+global.footer = "Blacky 🖤💝🦅"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
